b7b4813c192d8c3a4f3175a13ec4e4cff70f55ff
enoch <henokt828@gmail.com>
2025-07-19 14:02:18 +0300

    feat:finish the Webpack + React setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [{ loader: 'file-loader' }],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new Dotenv()
+  ]
+};
+// This Webpack configuration file is used to bundle the React application.
