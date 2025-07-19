7530a119050042b60431e345efea99dd5120f480
enoch <henokt828@gmail.com>
2025-07-19 18:53:31 +0300

    Set up React 18 root rendering in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client'; 
 import App from './App';
+import { Provider } from 'react-redux';
 import store from './redux/store';
+import { ThemeProvider } from '@emotion/react';
+import { lightTheme, darkTheme } from './theme';
+import { useState } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+// Wrap app in Root component to allow theme toggle
+const Root = () => {
+  const [isDark, setIsDark] = useState(false);
+  const theme = isDark ? darkTheme : lightTheme;
+
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App toggleTheme={() => setIsDark((prev) => !prev)} isDark={isDark} />
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container); 
+root.render(<Root />);
